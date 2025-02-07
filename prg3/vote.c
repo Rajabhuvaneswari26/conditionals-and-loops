ef512580e1b84f14afde447a0c5ddf0d8e3edb27
Rajabhuvaneswari26 <rajabhuvaneswarig@gmail.com>
2025-02-07 15:02:55 +0530

    Add files via upload

---
+++ prg3/vote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("Enter person's age:");
+scanf("%d",&age);
+if(age>18)
+{
+printf("The person is eligible to vote.");
+}
+else
+{
+printf("The person is not eligible to vote.");
+}
+return 0;
+}
